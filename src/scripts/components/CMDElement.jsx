bf3b47a4beab35bcf23c20773f57a80529023aa0
Mitchell Seaton <meaton.2v@gmail.com>
2015-03-17 16:29:20 +0100

    Added output for attributes list and fixed element enum display.

--- src/scripts/components/CMDElement.jsx
+++ src/scripts/components/CMDElement.jsx
@@ -11,42 +11,66 @@ var CMDElement = React.createClass({
     var lb = React.createElement('br');
     var minC = (elem.hasOwnProperty('@CardinalityMin')) ? elem['@CardinalityMin'] : 1;
     var maxC = (elem.hasOwnProperty('@CardinalityMax')) ? elem['@CardinalityMax'] : 1;
-    var docu_attr = (elem.hasOwnProperty("@Documentation")) ? elem['@Documentation'] : null;
-    var display_attr = (elem.hasOwnProperty("@DisplayPriority")) ? elem['@DisplayPriority'] : null;
-    var conceptLink_attr = (elem.hasOwnProperty("@ConceptLink")) ? [React.createElement("span", { className: "attrElem" }, elem['@ConceptLink']), lb] : null;
+    var docu_attr = (elem.hasOwnProperty("@Documentation")) ? [React.createElement("span", { className: "attrElem" }, "Documentation: " + elem['@Documentation']), lb] : null;
+    var display_attr = (elem.hasOwnProperty("@DisplayPriority")) ? [React.createElement("span", { className: "attrElem" }, "DisplayPriority: " + elem['@DisplayPriority']), lb] : null;
+    var conceptLink_attr = (elem.hasOwnProperty("@ConceptLink")) ? [React.createElement("span", { className: "attrElem" }, "ConceptLink: ", new React.createElement("a", { href: elem['@ConceptLink'] }, elem['@ConceptLink']) ), lb] : null;
     var multilingual_attr = (elem.hasOwnProperty('@Multilingual')) ? [React.createElement("span", { className: "attrElem" }, "Multilingual: " + elem['@Multilingual']), lb]: null;
     var card_attr = [React.createElement('span', { className: "attrElem" }, "Number of occurrences: " + minC + " - " + maxC), lb];
     return {conceptLink_attr, docu_attr, display_attr, card_attr, multilingual_attr};
   },
-  render: function () {
-    //TODO: Use ValueScheme and display enums
-    var valueScheme = this.props.elem['@ValueScheme'];
-    console.log('rendering element: ' + require('util').inspect(this.props.elem));
+  getValueScheme: function(obj) {
+    var valueScheme = obj['@ValueScheme'];
     console.log(typeof valueScheme);
+
     if(typeof valueScheme != "string") {
-      valueScheme = this.props.elem.ValueScheme;
-      if(valueScheme != undefined)
-        if(valueScheme.pattern != undefined)
+      valueScheme = obj.ValueScheme;
+
+      if(valueScheme != undefined) {
+        if(valueScheme.pattern != undefined) // attr or elem
           valueScheme = valueScheme.pattern;
-        else {
+        else { // elem
           var enumItems = (!$.isArray(valueScheme.enumeration.item)) ? [valueScheme.enumeration.item] : valueScheme.enumeration.item;
           valueScheme = (
             <DropdownButton bsSize="small" title={(enumItems.length > 0 && typeof enumItems[0] != "string") ? enumItems[0]['$'] : enumItems[0]}>
               {
-                $.map(valueScheme.enumeration.item, function(item, index) {
+                $.map(enumItems, function(item, index) {
                   return <MenuItem eventKey={index}>{(typeof item != "string" && item.hasOwnProperty('$')) ? item['$'] : item}</MenuItem>
                 })
               }
             </DropdownButton>
           );
         }
+      } else if(obj.Type != undefined) // attr
+          return obj.Type;
     }
+
+    return valueScheme;
+  },
+  render: function () {
+    var self = this;
+    var valueScheme = this.getValueScheme(this.props.elem);
+    console.log('rendering element: ' + require('util').inspect(this.props.elem));
+
+    var attrList = (this.props.elem.AttributeList != undefined) ? (
+      <div className="attrList">AttributeList:
+        <div className="attrAttr">
+        {
+          $.map(this.props.elem.AttributeList, function(attr) {
+            return attr.Name + " " + self.getValueScheme(attr);
+          })
+        }
+        </div>
+      </div>
+    ) : null;
+
     return (
       <div className="CMDElement">
-        <span>Element: </span>{this.props.elem['@name']} { valueScheme }
+        <span>Element: </span>
+        <b>{this.props.elem['@name']}</b> { valueScheme }
         <div className="elemAttrs">
           {this.elemAttrs(this.props.elem)}
         </div>
+        {attrList}
       </div>
       );
   }
