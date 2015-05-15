3155c63ef82fef5581e48566a6d5f53ee4cf9664
Mitchell Seaton <meaton.2v@gmail.com>
2015-05-15 15:16:15 +0200

    Test data for CCR table.

---
+++ src/scripts/CCRtestdata.js
@@ -0,0 +1,146 @@
+module.exports = [
+  {
+      "definition": "Quantifying expression functioning as a determiner or pronoun. This category refers to plural elements that are of masculine gender.",
+      "identifier": "QuantifyingDeterminerPlM",
+      "name": "Quantifying determiner (pl,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3596_a9f1a298-34f2-0ba1-cb5d-4871789e32f9",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Quantifying expression functioning as a determiner or pronoun. This category refers to singular elements that are of masculine gender.",
+      "identifier": "QuantifyingDeterminerSgM",
+      "name": "Quantifying determiner (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3594_9e51d835-ee60-b34b-f1ab-c7986678edb0",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Adjective that is specified for plural number and masculine gender.",
+      "identifier": "AdjectivePlM",
+      "name": "adjective (pl,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3611_8d80f30b-e461-77a0-ca99-43593e5275ca",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Adjective that is specified for singular number and masculine gender.",
+      "identifier": "AdjectiveSgM",
+      "name": "adjective (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3609_0a501089-8b15-ccba-5bbd-f8f0aad975f3",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Indefinite article which is used for elements that are plural in number and of masculine gender.",
+      "identifier": "IndefiniteArticlePlM",
+      "name": "indefinite article (pl,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3583_35d230a3-edc4-c086-3a72-cf857ef037a1",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Indefinite article which is used for elements that are singular in number and of masculine gender.",
+      "identifier": "IndefiniteArticleSgM",
+      "name": "indefinite article (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3585_62f83740-ae7c-cb24-7bd0-353457f57bbc",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Form of interrogative pronoun that is used to refer to plural referents that have masculine gender.",
+      "identifier": "InterrogativePronounPlM",
+      "name": "interrogative pronoun (pl,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3631_9583b4fe-0bb2-8f97-b456-829fa26e57b4",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Form of interrogative pronoun that is used to refer to singular referents that have masculine gender.",
+      "identifier": "InterrogativePronounSgM",
+      "name": "interrogative pronoun (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3629_413ac66f-ac9e-f5a4-13a5-1d390a0f9b12",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Form of personal pronoun that is used to refer to singular referents that have masculine gender.",
+      "identifier": "PersonalPronounSgM",
+      "name": "personal pronoun (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3619_4d963b57-167a-2303-2338-278a2dff2d97",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Form of possessive pronoun that is used to refer to plural referents that have masculine gender.",
+      "identifier": "PossessivePronounPlM",
+      "name": "possessive pronoun (pl,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3625_5a65af7e-289c-4843-c5a0-9e26cf5cc01b",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Form of possessive pronoun that is used to refer to singular referents that have masculine gender.",
+      "identifier": "PossessivePronounSgM",
+      "name": "possessive pronoun (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3623_31029fd9-6a65-0311-837f-42cea0b6e957",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Form of relative pronoun that is used to refer to plural referents that have masculine gender.",
+      "identifier": "RelativePronounPlM",
+      "name": "relative pronoun (pl,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3637_02602ec3-5fc7-0b5f-97bd-c7b0063317d9",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Form of relative pronoun that is used to refer to singular referents that have masculine gender.",
+      "identifier": "RelativePronounSgM",
+      "name": "relative pronoun (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3635_db8b9ad0-2aa2-e33b-cfe5-62fea96ce669",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "form of pronoun which can only be combined with a word referring to a man (natural gender) (source: CGN)",
+      "identifier": "person-3m",
+      "name": "person 3m",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-5003_9bdea7b4-fddc-b62b-ffd3-217a8963e5e0",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Determiner expressing negative quantity of a plural and masculine element.",
+      "identifier": "NegativeQuantifyingDeterminerPlM",
+      "name": "negative quantifying determiner (pl,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3605_3220511e-6566-fdcb-1da1-67f89bf61a00",
+      "type": "concept",
+      "version": "1:0"
+  },
+  {
+      "definition": "Determiner expressing negative quantity of a singular and masculine element.",
+      "identifier": "NegativeQuantifyingDeterminerSgM",
+      "name": "negative quantifying determiner (sg,m)",
+      "owner": "CLARIN",
+      "pid": "http://hdl.handle.net/11459/CCR_C-3603_03cc25dc-7f87-3896-0cfe-0dd2bb70d7f8",
+      "type": "concept",
+      "version": "1:0"
+  }
+];
