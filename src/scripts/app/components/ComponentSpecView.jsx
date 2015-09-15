b9d2e4fce8ab28c79020742189b195e7b2a79777
Twan Goosen <twan.goosen@mpi.nl>
2015-09-15 16:00:17 +0200

    Cleanup of spec view: merged render methods and removed unused parsing methods

--- src/scripts/app/components/ComponentSpecView.jsx
+++ src/scripts/app/components/ComponentSpecView.jsx
@@ -54,71 +54,8 @@ var ComponentSpec = React.createClass({
       domains: require('../../domains.js')
     };
   },
-//TODO flux: get rid of 'parsing' (in this component)
-  componentWillUpdate: function(nextProps, nextState) {
-    console.log(this.constructor.displayName, 'will update');
-    var self = this;
-    var newItem = nextProps.spec;
-    var prevItem = this.props.spec;
 
-    if(newItem != null && nextState.childComponents == null && nextState.childElements == null) {
-      if(newItem.CMD_Component.AttributeList != undefined && !$.isArray(newItem.CMD_Component.AttributeList.Attribute))
-        newItem = update(newItem, { CMD_Component: { AttributeList: { Attribute: { $set: [newItem.CMD_Component.AttributeList.Attribute] } }}});
-
-      this.parseComponent(newItem, nextState);
-    }
-  },
-
-  parseComponent: function(item, state) {
-    console.log('parseComponent');
-
-    var rootComponent = item.CMD_Component;
-    var childComponents = (!$.isArray(rootComponent.CMD_Component) && rootComponent.CMD_Component != null) ? [rootComponent.CMD_Component] : (rootComponent.CMD_Component||[]);
-    var childElements = (!$.isArray(rootComponent.CMD_Element) && rootComponent.CMD_Element != null) ? [rootComponent.CMD_Element] : (rootComponent.CMD_Element||[]);
-
-    console.log('child item components: ' + childComponents.length);
-    console.log('child item elements: ' + childElements.length);
-
-    if(childElements.length >= 1)
-      console.log('first item element: ' + childElements[0]['@name']);
-
-    for(var i=0; i < childComponents.length; i++)
-      if(childComponents[i].hasOwnProperty("@ComponentId"))
-      {
-        //TODO flux: allow for expansion of linked component (through action)
-      }
-        // this.loadComponent(childComponents[i]["@ComponentId"], "json", function(data) {
-        //     console.log('data child comp: ' + (data.CMD_Component != null));
-        //     data.CMD_Component = update(data.CMD_Component, { $merge: {'@CardinalityMin': (childComponents[i].hasOwnProperty("@CardinalityMin")) ? childComponents[i]["@CardinalityMin"] : 1, '@CardinalityMax': (childComponents[i].hasOwnProperty("@CardinalityMax")) ? childComponents[i]["@CardinalityMax"] : 1}})
-        //     childComponents[i] = update(data, {open: {$set: state.editMode}});
-        // });
-      else {
-        var isInlineComponent = (childComponents[i].Header == undefined);
-        childComponents[i] = update(childComponents[i], {open: {$set: (state.editMode || isInlineComponent)}})
-        console.log('childComponent: ' + JSON.stringify(childComponents[i]));
-      }
-
-    this.setState({ childElements: childElements, childComponents: childComponents,
-      //profile: (item['@isProfile'] === "true") ? item : state.profile,
-      //component: (item['@isProfile'] === "true") ? state.component : item,
-      registry: state.registry });
-  },
-  printProperties: function(item) {
-    var self = this;
-    var rootComponent = item.CMD_Component;
-
-    // Display properties
-    var conceptLink = (rootComponent && rootComponent['@ConceptLink'] != null) ? <li><span>ConceptLink:</span> <a href={rootComponent['@ConceptLink']}>{rootComponent['@ConceptLink']}</a></li> : null;
-    return (
-      <ul>
-        <li><span>Name:</span> <b>{item.Header.Name}</b></li>
-        <li><span>Description:</span> {item.Header.Description}</li>
-        {conceptLink}
-      </ul>
-    );
-  },
   render: function() {
-    var self = this;
     var item = this.props.spec;
 
     if(item == null)
@@ -127,15 +64,21 @@ var ComponentSpec = React.createClass({
       );
     else {
       var rootClasses = classNames({ ComponentViewer: true });
+      var rootComponent = item.CMD_Component;
+
+      // Display properties
+      var conceptLink = (rootComponent && rootComponent['@ConceptLink'] != null) ? <li><span>ConceptLink:</span> <a href={rootComponent['@ConceptLink']}>{rootComponent['@ConceptLink']}</a></li> : null;
+
       return (
           <div className={rootClasses}>
             {/*errors*/}
             <div className="rootProperties">
-              {this.printProperties(item)}
+              <ul>
+                <li><span>Name:</span> <b>{item.Header.Name}</b></li>
+                <li><span>Description:</span> {item.Header.Description}</li>
+                {conceptLink}
+              </ul>
             </div>
-            {/*attrList*/}
-            {/*childElem*/}
-            {/*childComp*/}
             <CMDComponentView spec={item.CMD_Component} />
           </div>
         );
