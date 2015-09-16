62e713950cbc639ce59ef81a00bd966798f441cf
Twan Goosen <twan.goosen@mpi.nl>
2015-09-16 15:16:46 +0200

    syntax in ExpansionState util

--- src/scripts/app/service/ExpansionState.js
+++ src/scripts/app/service/ExpansionState.js
@@ -3,12 +3,15 @@ var update = React.addons.update;
 
 module.exports = {
   isExpanded: function(state, itemId) {
-    return (state != undefined && state[itemId] != undefined && state[itemId] === true);
+    return (state != undefined
+            && state[itemId] != undefined
+            && state[itemId] === true);
   },
 
   setChildState: function(state, id, value) {
-    var change = {};
-    change[id] = {$set: value};
-    return update(state, change);
+    // 'update' object to set value of 'id' property to 'value'
+    return update(state, {
+      [id]: {$set: value}
+    });
   }
 }
