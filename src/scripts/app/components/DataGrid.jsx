a4d4b9d509ebc0a197f8d1ec8597113170f642d8
Twan Goosen <twan.goosen@mpi.nl>
2015-09-28 17:46:35 +0200

    cleaned up DataGrid source file

--- src/scripts/app/components/DataGrid.jsx
+++ src/scripts/app/components/DataGrid.jsx
@@ -58,48 +58,6 @@ var DataGrid = React.createClass({
 
   rowClick: function(val, target, addComponent) {
     this.props.onRowSelect(val, target);
-    // var self = this;
-    // var tableId = this.refs.wrapper.getDOMNode().id;
-    // var currentItem = this.state.lastSelectedItem;
-    //
-    // if(currentItem != null && currentItem != target && !this.state.multiSelect)
-    //   currentItem.setState({selected: false, active: false});
-    //
-    // console.log('addComponent:' + addComponent);
-    //
-    // this.setState(function(state, props) {
-    //   if(currentItem != target && !(currentItem == null && val === this.context.itemId) && target.state.selected) {
-    //     self.loadItem(state.currentType.substr(0, state.currentType.length-1), val);
-    //   } else if(addComponent && state.currentType == "components") {
-    //     console.log('add component: ' + target.refs.addButton.props.active);
-    //
-    //     self.props.component(val, target.refs.addButton.props.active);
-    //     target.setState({ active: false });
-    //   } else {
-    //     var selectedRows = $('#' + tableId + ' tr.selected:first');
-    //
-    //     if(selectedRows.length < 1)
-    //       self.props.profile(null); //TODO find next selection item in multiple select mode
-    //     else {
-    //       var id = selectedRows.data().reactid;
-    //       if(id != undefined && id.indexOf('clarin') > 0) {
-    //         id = id.substr(id.indexOf('$')+1, id.length).replace(/=1/g, '.').replace(/=2/g, ':');
-    //         self.loadItem(state.currentType.substr(0, state.currentType.length-1), id);
-    //       }
-    //     }
-    //     return { lastSelectedItem: null };
-    //   }
-    //
-    //   return  { lastSelectedItem: target };
-    // });
-  },
-
-  showProfile: function() {
-    //todo: trigger action
-  },
-
-  showComponent: function() {
-    //todo: trigger action
   }
 });
 
