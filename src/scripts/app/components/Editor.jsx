f2f66b856388543ddf15bb5a25ae2293b13f4488
Twan Goosen <twan.goosen@mpi.nl>
2015-10-09 16:19:12 +0200

    authentication check in editor

--- src/scripts/app/components/Editor.jsx
+++ src/scripts/app/components/Editor.jsx
@@ -1,6 +1,13 @@
 'use strict';
 
-var React = require('react');
+var log = require('loglevel');
+
+var React = require("react"),
+    Constants = require("../constants"),
+    Fluxxor = require("fluxxor"),
+    FluxMixin = Fluxxor.FluxMixin(React),
+    StoreWatchMixin = Fluxxor.StoreWatchMixin;
+
 var {RouteHandler, Navigation} = require('react-router');
 
 require('../../../styles/ComponentEditor.sass');
@@ -10,14 +17,48 @@ require('../../../styles/ComponentEditor.sass');
 * @constructor
 */
 var Editor = React.createClass({
+  mixins: [FluxMixin, StoreWatchMixin("AuthenticationStore")],
+
+  // Required by StoreWatchMixin
+  getStateFromFlux: function() {
+    var flux = this.getFlux();
+    return {
+      auth: flux.store("AuthenticationStore").getState()
+    };
+  },
+
+  componentDidMount: function() {
+    if(!this.isAuthenticated()) {
+      // try to trigger a login
+      var loginForm = $("form.login-form");
+      // we're assuming that the AuthState component is rendered somehwere on the page
+      log.info(loginForm);
+      if(loginForm.length) {
+        loginForm.submit();
+      } else {
+        alert("Please authenticate manually by pressing the login button");
+        log.warn("Could not trigger login, login form not found on page");
+      }
+    }
+  },
+
   render: function () {
-    return (
-      <section id="editor">
-        <h2>Component Editor</h2>
-        <RouteHandler/>
-        //grid
-      </section>
-      );
+    if(this.isAuthenticated()) {
+      return (
+        <section id="editor">
+          <h2>Component Editor</h2>
+          <RouteHandler/>
+          {/*grid*/}
+        </section>
+        );
+    } else {
+      // on mount, login should be triggered
+      return <div>Login required to edit</div>;
+    }
+  },
+
+  isAuthenticated: function() {
+    return this.state.auth.authState.authenticated;
   }
 });
 
