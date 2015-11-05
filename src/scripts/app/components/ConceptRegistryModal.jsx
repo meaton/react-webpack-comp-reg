6e7cb0893dbb818a4c43ce45329b283572a7d5fe
Twan Goosen <twan.goosen@mpi.nl>
2015-11-05 10:03:55 +0100

    added copied and modified components for showing concept link modal

---
+++ src/scripts/app/components/ConceptRegistryModal.jsx
@@ -0,0 +1,201 @@
+'use strict';
+
+var React = require('react/addons');
+var Table = require('reactabular').Table;
+var sortColumn = require('reactabular').sortColumn;
+
+//mixins
+var LinkedStateMixin = React.addons.LinkedStateMixin;
+
+//bootstrap mixins
+var OverlayMixin = require('react-bootstrap/lib/OverlayMixin');
+
+//bootstrap
+var Modal = require('react-bootstrap/lib/Modal');
+var Input = require('react-bootstrap/lib/Input');
+var Glyphicon = require('react-bootstrap/lib/Glyphicon');
+var Button = require('react-bootstrap/lib/Button');
+
+//utils
+var update = React.addons.update;
+var classNames = require('classnames');
+
+require('../../../styles/EditorDialog.sass');
+/**
+* ConceptRegistryModal - Bootstrap Modal dialog for setting the Concept Registry (CCR) link.
+* @constructor
+* @mixes Loader
+* @mixes React.addons.LinkedStateMixin
+*/
+var ConceptRegistryModal = React.createClass({
+  mixins: [//CompRegLoader,
+    LinkedStateMixin],
+  propTypes: {
+    onSelect: React.PropTypes.func.isRequired,
+    container: React.PropTypes.object.isRequired
+  },
+  getInitialState: function() {
+    return {
+      data: [],
+      columns: [],
+      inputSearch: "",
+      currentLinkSelection: null,
+    }
+  },
+  getDefaultProps: function() {
+    return {
+      title: "Search in CLARIN Concept Registry"
+    };
+  },
+  inputSearchUpdate: function(evt) {
+    console.log('search query: ' + this.state.inputSearch);
+    var self = this;
+    this.setState({ data: [], currentLinkSelection: null });
+    this.queryCCR(this.state.inputSearch, function(data) {
+      if(data != null)
+        self.setState({ data: data });
+    });
+  },
+  handleEnter: function(evt) {
+    if(evt.keyCode == 13) {
+      console.log('enter: ' + this.state.inputSearch);
+      this.inputSearchUpdate();
+    }
+  },
+  confirm: function(evt) {
+    var selectedValue = (this.state.currentLinkSelection != null) ? this.state.data[this.state.currentLinkSelection].pid : "";
+    this.assignValue(selectedValue);
+    this.close();
+  },
+  clear: function(evt) {
+    this.assignValue("");
+    this.close();
+  },
+  close: function(evt) {
+    if(evt) evt.stopPropagation();
+    var elementId="";//TODO!
+    if(elementId) React.unmountComponentAtNode(document.getElementById(elementId));
+    else console.error('Cannot unmount Alert dialog: ', elementId);
+  },
+  assignValue: function(value) {
+    this.props.onSelect(value);
+    // var target = this.props.target;
+    // if(target.refs.conceptRegInput != undefined)
+    //   target.refs.conceptRegInput.props.onChange(value);
+    // else if(target.constructor.displayName === "TypeModal")
+    //   this.props.onClose(value)
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if(prevState.currentLinkSelection != this.state.currentLinkSelection) {
+      var selectedClass = "selected";
+      var tbody = $('#' + this.refs.table.getDOMNode().id + " tbody");
+      tbody.children('.' + selectedClass).toggleClass(selectedClass);
+      tbody.children().eq(this.state.currentLinkSelection).toggleClass(selectedClass);
+    }
+
+    // if(prevState.data.length != this.state.data.length)
+    //   this.props.onChange(this.getDOMNode());
+  },
+  componentWillUnmount: function() {
+    $(document.body).off('keydown', this.handleEnter);
+  },
+  componentWillMount: function() {
+    $(document.body).on('keydown', this.handleEnter);
+
+    var defaultClick = this.handleCellClick;
+    var defaultProps = this.defaultCellProps;
+
+    this.setState({ columns: [
+      {
+        property: 'name',
+        header: 'Name',
+        cell: defaultClick
+      },
+      {
+        property: 'definition',
+        header: 'Definition',
+        cell: defaultClick
+      },
+      {
+        property: 'identifier',
+        header: 'Identifier',
+        cell: defaultClick
+      },
+      {
+        property: 'owner',
+        header: 'Owner',
+        cell: defaultClick
+      },
+      {
+        property: 'pid',
+        header: 'PersistentId',
+        cell: function(value, data, rowIndex) {
+          return {
+            value: (value) ? (<span><a href={value} target="_blank">{value}</a></span>) : "",
+            props: defaultProps(rowIndex)
+          }
+        }
+      },
+      {
+        property: 'type',
+        header: 'Type',
+        cell: this.handleCellClick
+      },
+      {
+        property: 'version',
+        header: 'Version',
+        cell: this.handleCellClick
+      }
+    ]});
+  },
+  componentDidMount: function() {
+    //this.props.onChange(this.getDOMNode());
+  },
+  defaultCellProps: function(rowIndex) {
+    var self = this;
+    var isEven = (rowIndex % 2);
+
+    return {
+      onClick: function(evt) {
+        self.setState({ currentLinkSelection: rowIndex });
+      },
+      className: classNames({ odd: !isEven, even: isEven })
+    };
+  },
+  handleCellClick: function(value, data, rowIndex) {
+    return {
+      value: value,
+      props: this.defaultCellProps(rowIndex)
+    };
+  },
+  loadTestdata: function() {
+    this.setState({ data: testdata, currentLinkSelection: null });
+  },
+  render: function() {
+    var self = this;
+    var tableClasses = classNames('table', 'table-bordered', 'table-hover', 'table-striped', 'table-condensed');
+    var conceptRegHeader = {
+      onClick: function(col) {
+        sortColumn(self.state.columns, col, self.state.data, self.setState.bind(self));
+      }
+    };
+
+    return (
+      <Modal ref="modal" id="ccrModal" key="ccrModal" className="registry-dialog" title={this.props.title} backdrop={false} animation={false} onRequestHide={this.close} container={this.props.container}>
+        <div className='modal-body'>
+          <Input type="text" placeholder="Type keyword and press Enter to search" valueLink={this.linkState('inputSearch')} addonBefore={<Glyphicon glyph='search' />} buttonAfter={<Button onClick={this.inputSearchUpdate}>Search</Button>}/>
+          <Table id="ccrTable" ref="table" columns={this.state.columns} data={this.state.data} header={conceptRegHeader} className={tableClasses} />
+        </div>
+        <div className="modal-footer">
+          <Button onClick={this.confirm} disabled={this.state.currentLinkSelection == null}>Ok</Button>
+          <Button onClick={this.clear}
+            > {/*className={classNames({ 'hide': (this.props.target.refs.conceptRegInput == undefined) })}*/}
+            Clear Setting</Button>
+          <Button onClick={this.close}>Cancel</Button>
+        </div>
+      </Modal>
+    );
+  }
+});
+
+module.exports = ConceptRegistryModal;
---
+++ src/scripts/app/components/ModalTrigger.jsx
@@ -0,0 +1,123 @@
+'use strict';
+var log = require('loglevel');
+
+var React = require('react/addons');
+var Draggable = require('react-draggable');
+
+//bootstrap mixins
+var OverlayMixin = require('react-bootstrap/lib/OverlayMixin');
+
+//bootstrap
+var Button = require('react-bootstrap/lib/Button');
+
+//utils
+var update = React.addons.update;
+
+/**
+* ModalTrigger - Bootstrap custom ModalTrigger utilising react-bootstrap OverlayMixin. Manages dialog display for two components implementing Bootstrap Modal, TypeModal and ConceptRegistryModal.
+* @constructor
+* @mixes OverlayMixin
+*/
+var ModalTrigger = React.createClass({
+  mixins: [OverlayMixin],
+  propTypes: {
+    modal: React.PropTypes.object.isRequired,
+    label: React.PropTypes.string.isRequired,
+    useLink: React.PropTypes.bool,
+  },
+  getDefaultProps: function() {
+    return {
+      useLink: false
+    }
+  },
+  getInitialState: function() {
+    return {
+      isModalOpen: false,
+      position: {
+        top: 0, left: 0
+      }
+    };
+  },
+  toggleModal: function(evt) {
+      log.debug('modal visible: ', this.state.isModalOpen, this.props.container);
+
+      var offset = $(this.props.container.getDOMNode()).position();
+      //TODO: correct offset
+      offset.top += (this.props.container.getDOMNode().className.indexOf("editor") != -1) ? $('.ComponentViewer').offset().top : 0;
+      log.debug('toggle modal offset:', offset.top, offset.left);
+
+      this.setState({
+        position: (!this.state.isModalOpen) ? update(this.state.position, { $set: offset }) : { top: 0, left: 0 },
+        isModalOpen: !this.state.isModalOpen
+      });
+  },
+  handleStart: function (event, ui) {
+      console.log('Event: ', event);
+      console.log('Position: ', ui.position);
+  },
+  handleDrag: function (event, ui) {
+      console.log('Event: ', event);
+      console.log('Position: ', ui.position);
+      var offset = $(this.state.container.getDOMNode()).position();
+      this.setState({
+        position: update(this.state.position, { $set: { top: ui.position.top + offset.top, left: ui.position.left + offset.left }})
+      });
+  },
+  handleStop: function (event, ui) {
+      console.log('Event: ', event);
+      console.log('Position: ', ui.position);
+  },
+  handleTableScroll: function(domNode) {
+    var modal = domNode;
+    var tableBody = $(modal).find('.table tbody').eq(0);
+    var tableHead = $(modal).find('.table thead').eq(0);
+    var scrollbarWidth = tableBody.innerWidth() - tableBody.prop('scrollWidth');
+
+    if(tableBody.innerWidth()-1 > tableBody.prop('scrollWidth')) {
+      tableHead.width(tableBody.innerWidth() - scrollbarWidth);
+      $(modal).find('.table').addClass('with-scroll');
+    } else {
+      tableHead.width('100%');
+      $(modal).find('.table').removeClass('with-scroll');
+    }
+  },
+  componentDidUpdate: function() {
+    var overlayNode = this.getOverlayDOMNode();
+    if(overlayNode == undefined) overlayNode = "#"
+    $(this.getOverlayDOMNode()).css({left: this.state.position.left, top: this.state.position.top, display: (this.state.isModalOpen) ? 'block' : 'none'});
+  },
+  componentDidMount: function() {
+    console.log('container: ' + this.state.container);
+  },
+  render: function() {
+    if(this.props.useLink)
+      return (
+        <a onClick={this.toggleModal}>{this.props.label}</a>
+      )
+    else
+      return (
+        <Button onClick={this.toggleModal}>
+          {this.props.label}
+        </Button>
+      );
+  },
+  renderOverlay: function () {
+    if(!this.state.isModalOpen) {
+      return <span/>;
+    } else {
+      log.trace("Modal active", this.props.modal);
+      return (
+        <Draggable axis="both" handle=".modal-header"
+          grid={[5, 5]}
+          zIndex={1050}
+          onStart={this.handleStart}
+          onDrag={this.handleDrag}
+          onStop={this.handleStop}>
+          {this.props.modal}
+        </Draggable>
+      );
+    }
+  }
+});
+
+module.exports = ModalTrigger;
