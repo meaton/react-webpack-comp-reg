48aced04f0c16602a32700669b73d77dda05b3b7
Twan Goosen <twan.goosen@mpi.nl>
2015-12-09 10:08:18 +0100

    fixed dialogue rendering in ReactAlert

--- src/scripts/app/util/ReactAlert.jsx
+++ src/scripts/app/util/ReactAlert.jsx
@@ -1,4 +1,5 @@
 'use strict';
+var log = require('loglevel');
 
 var React = require('react');
 var ReactDOM = require('react-dom');
@@ -10,66 +11,60 @@ var Modal = require('react-bootstrap/lib/Modal');
 
 module.exports = {
 
-  renderAlert: function(instance, elementId) {
-    var div = React.DOM.div;
-    if(instance && elementId)
-      ReactDOM.render(div({ className: 'static-modal' }, instance), document.getElementById(elementId));
-    else
-      log.error('Cannot render Alert dialog: ', elementId);
-  },
-
-  closeAlert: function(elementId, evt) {
-    if(evt) evt.stopPropagation();
-    if(elementId)
-      React.unmountComponentAtNode(document.getElementById(elementId));
-    else
-      log.error('Cannot unmount Alert dialog: ', elementId);
-  },
-
   showMessage: function(container, title, message) {
-    this.showAlert(function(closeAlert) { return (
-      <Modal title={title}
-        enforceFocus={true}
-        backdrop={true}
-        animation={false}
-        container={container}
-        onRequestHide={closeAlert}>
-        <div className="modal-body">
-          <div className="modal-desc">
-            <div>{message}</div>
-          </div>
-        </div>
-        <div className="modal-footer">
-          <Button onClick={closeAlert}>Ok</Button>
-        </div>
-      </Modal>
-    )});
+    log.debug("Alert: [", title, "]", message);
+
+    var renderBodyContent = function() { return (
+      <div className="modal-desc">
+        <div>{message}</div>
+      </div>
+    )};
+
+    var renderFooterContent = function(opts) { return (
+      <Button onClick={opts.closeAlert}>Ok</Button>
+    )};
+
+    this.showModalAlert(title, renderBodyContent, renderFooterContent);
   },
 
   showConfirmationDialogue: function(container, title, message, onYes, onNo) {
-    this.showAlert(function(closeAlert) { return (
-      <Modal title={title}
-        enforceFocus={true}
-        backdrop={true}
-        animation={false}
-        container={container}
-        onRequestHide={closeAlert}>
-        <div className="modal-body">
-          <div className="modal-desc">
-            <div>{message}</div>
-          </div>
-        </div>
-        <div className="modal-footer">
-          <Button onClick={function(evt) {
-              closeAlert(evt);
-              if(onYes) onYes();
-            }}>Yes</Button>
-          <Button onClick={function(evt) {
-              closeAlert(evt);
-              if(onNo) onNo();
-            }}>No</Button>
-        </div>
-      </Modal>
+
+    var renderBodyContent = function() { return (
+      <div className="modal-desc">
+        <div>{message}</div>
+      </div>
+    )};
+
+    var renderFooterContent = function(opts) { return (
+      <div>
+        <Button onClick={function(evt) {
+            opts.closeAlert(evt);
+            if(onYes) onYes();
+          }}>Yes</Button>
+        <Button onClick={function(evt) {
+            opts.closeAlert(evt);
+            if(onNo) onNo();
+          }}>No</Button>
+      </div>
+    )};
+
+    this.showModalAlert(title, renderBodyContent, renderFooterContent);
+  },
+
+  showModalAlert: function(title, renderBodyContent, renderFooterContent) {
+    this.showAlert(function(closeAlert) {
+      var opts = {
+        closeAlert: closeAlert
+      };
+
+      return (
+        <Modal.Dialog enforceFocus={true} backdrop={true}>
+          <Modal.Header closeButton={true} onHide={closeAlert}>
+            <Modal.Title>{title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{renderBodyContent(opts)}</Modal.Body>
+          <Modal.Footer>{renderFooterContent(opts)}</Modal.Footer>
+        </Modal.Dialog>
     )});
   },
 
@@ -84,5 +79,22 @@ module.exports = {
     this.renderAlert(dialogue, "alert-container");
 
     return dialogue;
+  },
+
+  renderAlert: function(instance, elementId) {
+    log.debug("Render alert at", elementId, instance);
+    var div = React.DOM.div;
+    if(instance && elementId)
+      ReactDOM.render(div({ className: 'static-modal' }, instance), document.getElementById(elementId));
+    else
+      log.error('Cannot render Alert dialog: ', elementId);
+  },
+
+  closeAlert: function(elementId, evt) {
+    if(evt) evt.stopPropagation();
+    if(elementId)
+      ReactDOM.unmountComponentAtNode(document.getElementById(elementId));
+    else
+      log.error('Cannot unmount Alert dialog: ', elementId);
   }
 }
