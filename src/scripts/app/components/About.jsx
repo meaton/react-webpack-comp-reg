15fab665a0dd31c05adf6cd1786b2178d4f623b5
Twan Goosen <twan.goosen@mpi.nl>
2016-01-06 18:19:43 +0100

    licence text styling

--- src/scripts/app/components/About.jsx
+++ src/scripts/app/components/About.jsx
@@ -40,12 +40,12 @@ var Main = React.createClass({
         </table>
         <p>Go to <a href="https://www.clarin.eu/cmdi">www.clarin.eu/cmdi</a> for more information.<br/>
         Email <a href="mailto:cmdi@clarin.eu">cmdi@clarin.eu</a> for questions/support.</p>
-        <p className="licence">
-          Copyright (C) 2016 CLARIN ERIC<br/>
-          This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.<br/>
-          This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.<br/>
-          You should have received a copy of the GNU General Public License along with this program. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
-        </p>
+        <div className="licence">
+          <p>Copyright (C) 2016 CLARIN ERIC</p>
+          <p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p>
+          <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</p>
+          <p>You should have received a copy of the GNU General Public License along with this program. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
+        </div>
       </div>
     );
   }
