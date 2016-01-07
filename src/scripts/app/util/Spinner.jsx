6347b7757db1af5a1876b0d79a530e1606070524
Twan Goosen <twan.goosen@mpi.nl>
2016-01-07 11:46:47 +0100

    switched to "fading circle" spinner

--- src/scripts/app/util/Spinner.jsx
+++ src/scripts/app/util/Spinner.jsx
@@ -2,8 +2,8 @@
 
 var React = require("react");
 
-//stylesheet
-require('spinkit/scss/spinners/8-circle.scss');
+//stylesheet - other options are available, see author's website for details
+require('spinkit/scss/spinners/10-fading-circle.scss');
 
 /**
 * Spinner - Component based on spinkit, see <http://tobiasahlin.com/spinkit/>
@@ -12,20 +12,21 @@ require('spinkit/scss/spinners/8-circle.scss');
 var Spinner = React.createClass({
   render: function() {
     return(
-      <div className="spinner sk-circle">
-        <div className="sk-circle1 sk-child"></div>
-        <div className="sk-circle2 sk-child"></div>
-        <div className="sk-circle3 sk-child"></div>
-        <div className="sk-circle4 sk-child"></div>
-        <div className="sk-circle5 sk-child"></div>
-        <div className="sk-circle6 sk-child"></div>
-        <div className="sk-circle7 sk-child"></div>
-        <div className="sk-circle8 sk-child"></div>
-        <div className="sk-circle9 sk-child"></div>
-        <div className="sk-circle10 sk-child"></div>
-        <div className="sk-circle11 sk-child"></div>
-        <div className="sk-circle12 sk-child"></div>
-      </div>);
+      <div className="spinner sk-fading-circle">
+        <div className="sk-circle1 sk-circle"></div>
+        <div className="sk-circle2 sk-circle"></div>
+        <div className="sk-circle3 sk-circle"></div>
+        <div className="sk-circle4 sk-circle"></div>
+        <div className="sk-circle5 sk-circle"></div>
+        <div className="sk-circle6 sk-circle"></div>
+        <div className="sk-circle7 sk-circle"></div>
+        <div className="sk-circle8 sk-circle"></div>
+        <div className="sk-circle9 sk-circle"></div>
+        <div className="sk-circle10 sk-circle"></div>
+        <div className="sk-circle11 sk-circle"></div>
+        <div className="sk-circle12 sk-circle"></div>
+      </div>
+    );
   }
 });
 
