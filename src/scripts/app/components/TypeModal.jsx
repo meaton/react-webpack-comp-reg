31c64be5790857752f134f3e29038e78f079f879
Twan Goosen <twan.goosen@mpi.nl>
2016-04-15 09:40:11 +0200

    Determine duplicate vocab values and include in error message

--- src/scripts/app/components/TypeModal.jsx
+++ src/scripts/app/components/TypeModal.jsx
@@ -97,10 +97,14 @@ var TypeModal = React.createClass({
 
       //check for duplicates
       if(_.uniq(itemValues).length != itemValues.length) {
+        var duplicates = _.keys(_.pick(_.countBy(itemValues), function(value){
+          return value > 1;
+        }));
+        log.debug("Value counts", duplicates);
         // use countby? https://github.com/lodash/lodash/blob/3.10.1/doc/README.md#_countbycollection-iteratee_identity-thisarg
         // var duplicates = _.difference(itemValues, uniqueItems);
         // log.debug("Duplicate items:", duplicates);
-        alert("Invalid vocabulary: all items in a vocabulary should have a unique value. Please remove duplicate values and try again");//.\n\n" + duplicates);
+        alert("Invalid vocabulary: all items in a vocabulary should have a unique value. Please remove these duplicate values and try again:\n\n" + duplicates);
         return;
       }
       //check for duplicate items
