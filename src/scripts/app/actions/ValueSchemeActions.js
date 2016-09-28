9350e02a3fde3a69cf5b41d6034a6bba75832acf
Twan Goosen <twan.goosen@mpi.nl>
2016-09-28 14:26:45 +0200

    loadValueScheme action

--- src/scripts/app/actions/ValueSchemeActions.js
+++ src/scripts/app/actions/ValueSchemeActions.js
@@ -7,9 +7,37 @@ var Constants = require("../constants");
 var ValueSchemeActions = {
   loadValueScheme: function(element) {
     log.debug("Loading value scheme for element", element);
-    //TODO
-    this.dispatch(Constants.LOAD_VALUE_SCHEME);
+
+    var vocabulary = null;
+    var type = null;
+    var pattern = null;
+
+    if(element.ValueScheme) {
+      if(element.ValueScheme.Vocabulary) {
+        vocabulary = deepCopy(element.ValueScheme.Vocabulary);
+      } else if(element.ValueScheme.pattern) {
+        pattern = element.ValueScheme.pattern.valueOf();
+      }
+    } else if(element['@ValueScheme']) {
+      type = element['@ValueScheme'].valueOf();
+    }
+
+    if(!(vocabulary || type || pattern)) {
+      log.error("No value scheme in element", element);
+    }
+
+    this.dispatch(Constants.LOAD_VALUE_SCHEME, {
+      vocabulary: vocabulary,
+      type: type,
+      pattern: pattern
+    });
   }
 };
 
 module.exports = ValueSchemeActions;
+
+function deepCopy(obj) {
+  if(obj != null) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+}
