b859e3c25d3d8ebaa6024993512beae4acb4baf9
Twan Goosen <twan.goosen@mpi.nl>
2016-10-03 17:19:53 +0200

    table layout tweaks

--- src/scripts/app/components/VocabularyEditor.jsx
+++ src/scripts/app/components/VocabularyEditor.jsx
@@ -226,13 +226,15 @@ function fixVocabTableColumnSizes() {
       $bodyCells = $table.find('tbody tr:first').children(),
       colWidth;
 
-  // Get the tbody columns width array
-  colWidth = $bodyCells.map(function() {
-      return $(this).width();
-  }).get();
+  if($bodyCells && $bodyCells.length > 0) {
+    // Get the tbody columns width array
+    colWidth = $bodyCells.map(function() {
+        return $(this).width();
+    }).get();
 
-  // Set the width of thead columns
-  $table.find('thead tr').children().each(function(i, v) {
-      $(v).width(colWidth[i]);
-  });
+    // Set the width of thead columns
+    $table.find('thead tr').children().each(function(i, v) {
+        $(v).width(colWidth[i]);
+    });
+  }
 }
