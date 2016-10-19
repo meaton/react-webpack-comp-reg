c82504432aaab76297e23afab452cedd8f058015
Twan Goosen <twan.goosen@mpi.nl>
2016-10-19 10:57:55 +0200

    Input, controls and handlers for documentaiton language code dialogue

--- src/scripts/app/components/editor/DocumentationInput.jsx
+++ src/scripts/app/components/editor/DocumentationInput.jsx
@@ -8,22 +8,31 @@ var update = require('react-addons-update');
 var ModalTrigger = require('../ModalTrigger');
 
 //bootstrap
-var Input = require('react-bootstrap/lib/Input');
 var Glyphicon = require('react-bootstrap/lib/Glyphicon');
+var Input = require('react-bootstrap/lib/Input');
+var Button = require('react-bootstrap/lib/Button');
+var Modal = require('react-bootstrap/lib/Modal');
 
 /**
 * ConceptLinkInput - Text input with button to trigger CCR search
 *
 * @constructor
 */
-var ConceptLinkInput = React.createClass({
+var DocumentationInput = React.createClass({
   propTypes: {
     value: React.PropTypes.array.isRequired,
     onChange: React.PropTypes.func.isRequired
   },
 
+  updateLanguageCode: function(index, value) {
+    log.debug("Update language code", index, value);
+  },
+
+  updateValue: function(index, evt) {
+    log.debug("Update documentation", index, evt);
+  },
+
   render: function() {
-    //TODO: Add language selector
     var {value, onChange, ...other} = this.props;
     var docs = ($.isArray(value) && value.length > 0) ? value : [null];
 
@@ -31,25 +40,33 @@ var ConceptLinkInput = React.createClass({
       <div>
       {
         docs.map(function(doc,index) {
-          var language = doc != null && doc['@lang'];
+          var languageCode = doc && doc['@lang'] || null;
+
+          var modalRef = "modalTrigger" + index;
+          var closeModal = this.closeDialogue.bind(this, modalRef);
+
           var languageInput = <ModalTrigger
-            ref="modalTrigger"
+            ref={modalRef}
             modalTarget="documentationLanguageModalContainer"
-            label={language || <Glyphicon glyph="comment"/>}
+            label={languageCode || <Glyphicon glyph="comment"/>}
             onOpen={this.props.loadValueSchemeData}
             useLink={true}
             modal={
               <DocumentationLanguageModal
-                language={language}
-                onClose={function(){log.debug("close");}}
-                onChange={function(){log.debug("change");}}
+                languageCode={languageCode}
+                onClose={closeModal}
+                onChange={this.updateLanguageCode.bind(this, index)}
                 />
             } />
-          return <Input key={index} type="text" value={doc == null ? "" : doc['$']} {...other} onChange={this.onChange.bind(this, index)} addonAfter={languageInput} />
+          return <Input key={index} type="text" value={doc == null ? "" : doc['$']} {...other} onChange={this.updateValue.bind(this, index)} addonAfter={languageInput} />
         }.bind(this))
       }
       </div>
     );
+  },
+
+  closeDialogue: function(modalRef, evt) {
+    this.refs[modalRef].toggleModal(evt);
   },
 
   onChange: function(index, e) {
@@ -71,24 +88,61 @@ var ConceptLinkInput = React.createClass({
     if(index >= currentDocs.length) {
       newDocs = update(currentDocs, {$push: [newValue]});
     } else {
-      newDocs= update(currentDocs, {0: {$set: newValue}});
+      newDocs = update(currentDocs, {0: {$set: newValue}});
     }
 
     this.props.onChange(newDocs);
   }
 });
 
+/**
+ * DocumentationLanguageModal Modal with local state initialised on passed language code value and user triggered callback to pass currently entered value
+ * @type {[type]}
+ */
 var DocumentationLanguageModal = React.createClass({
   propTypes: {
-    language: React.PropTypes.string,
+    languageCode: React.PropTypes.string,
     onChange: React.PropTypes.func.isRequired,
     onClose: React.PropTypes.func.isRequired
   },
 
+  getInitialState: function() {
+    return {
+      languageCode: this.props.languageCode || ""
+    }
+  },
+
+  onChange: function() {
+    this.props.onChange(this.state.languageCode);
+    this.props.onClose();
+  },
+
   render: function() {
-    return <div>bla</div>;
+
+    return (
+      <Modal.Dialog ref="modal" id="typeModal" key="typeModal" className="type-dialog" enforceFocus={true} backdrop={false}>
+
+        <Modal.Header closeButton={true} onHide={this.props.onClose}>
+          <Modal.Title>Documentation</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Input type="text" ref="languageCodeInput" value={this.state.languageCode} label="Language code" labelClassName="editorFormLabel" wrapperClassName="editorFormField"
+            onChange={function(evt) {
+              this.setState({languageCode: evt.target.value});
+            }.bind(this)}
+            />
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button onClick={this.onChange}>Ok</Button>
+          <Button onClick={this.props.onClose}>Cancel</Button>
+        </Modal.Footer>
+
+      </Modal.Dialog>
+    );
   }
 });
 
 
-module.exports = ConceptLinkInput;
+module.exports = DocumentationInput;
