b4b15319887e3b9f4c5211a89b8fa7ac91a932e5
Twan Goosen <twan.goosen@mpi.nl>
2016-11-18 10:49:03 +0100

    (de)serialization using papaparse

--- src/scripts/app/service/VocabularyCsvService.js
+++ src/scripts/app/service/VocabularyCsvService.js
@@ -1,18 +1,21 @@
 'use strict';
 var log = require('loglevel');
+var papaparse = require('papaparse');
 
 var VocabularyCsvService = {
 
   serializeItems: function(items) {
     log.debug("Serializing items to csv", items);
-    //TODO
-    return "items";
+    var data = papaparse.unparse(items);
+    log.debug("Serialized items:", data);
+    return data;
   },
 
   deserializeItems: function(data) {
     log.debug("Deserializing items from csv: ", data);
-    //TODO
-    return [{'$': 'test'}];
+    var items = papaparse.parse(data);
+    log.debug("Deserialized items", items);
+    return items;
   }
 }
 
